27779ddc59d6b1d88bf6e8af248770de40bef09c
Pavel Troshin <troshpaw@gmail.com>
2024-04-24 18:22:51 +0300

    finish_9

--- 9-alternating-case.js
+++ 9-alternating-case.js
@@ -2,7 +2,16 @@
 // https://www.codewars.com/kata/56efc695740d30f963000557/
 
 String.prototype.toAlternatingCase = function () {
-    return this + this;
+    let outputString = "";
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] === this[i].toUpperCase()) {
+            //console.log((this[i]), " ", this[i].toUpperCase());
+            outputString += this[i].toLowerCase();
+        } else {
+            outputString += this[i].toUpperCase();
+        }
+    }
+    return outputString;
 }
 
 let string = 'altERnaTIngcAsE';
